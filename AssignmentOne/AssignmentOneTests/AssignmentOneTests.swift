06bbf7e13f955cbff542112d033f84b30bc8b514
Jennifer-Wei Lin <jennifer-wei.lin@griffithuni.edu.au>
2023-03-31 11:42:29 +1000

    tests

--- AssignmentOne/AssignmentOneTests/AssignmentOneTests.swift
+++ AssignmentOne/AssignmentOneTests/AssignmentOneTests.swift
@@ -9,10 +9,37 @@ import XCTest
 
 final class AssignmentOneTests: XCTestCase {
 
-    func testExample() throws {
-        let ListRowView = ListRowView(item: ["Milk","checkmark.circle"])
-        XCTAssertEqual(ListRowView.item[0], "Milk")
-        XCTAssertEqual(ListRowView.item[1], "checkmark.circle")
+    //function for testing the items struct
+    func testItems() throws {
+        let item1 = Items(item: "Milk", checkedStatus: "unchecked")
+        let item2 = Items(item: "Cheese", checkedStatus: "checked")
+        
+        XCTAssert(item1.item == "Milk")
+        XCTAssert(item1.checkedStatus == "unchecked")
+        
+        XCTAssert(item2.item == "Cheese")
+        XCTAssert(item2.checkedStatus == "checked")
     }
-
-}
+    
+    //function for testing the checklist struct
+    func testChecklist() throws {
+        let item1 = Items(item: "Milk", checkedStatus: "unchecked")
+        let item2 = Items(item: "Cheese", checkedStatus: "checked")
+        let checklist1 = Checklist(checklist: "Groceries", items: [item1, item2])
+        
+        XCTAssert(checklist1.checklist == "Groceries")
+        XCTAssert(checklist1.items.count == 2)
+        XCTAssert(checklist1.items.contains(item1))
+        XCTAssert(checklist1.items.contains(item2))
+    }
+    
+    //function for testing the datamodel struct
+    func testDataModel() throws {
+        let item1 = Items(item: "Buy milk", checkedStatus: "unchecked")
+        let item2 = Items(item: "Pay bills", checkedStatus: "checked")
+        let checklist1 = Checklist(checklist: "Groceries", items: [item1, item2])
+        let dataModel = DataModel(checklists: [checklist1])
+        
+        XCTAssert(dataModel.checklists.count == 1)
+        XCTAssert(dataModel.checklists.contains(checklist1))
+    }}
