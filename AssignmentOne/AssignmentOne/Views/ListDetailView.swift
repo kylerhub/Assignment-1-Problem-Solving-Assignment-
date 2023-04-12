766f29767eebe66097105695194bc636e2e761b2
Jennifer-Wei Lin <jennifer-wei.lin@griffithuni.edu.au>
2023-04-12 14:28:49 +1000

    listDetailView loading

--- AssignmentOne/AssignmentOne/Views/ListDetailView.swift
+++ AssignmentOne/AssignmentOne/Views/ListDetailView.swift
@@ -13,66 +13,73 @@ struct ListDetailView: View {
 
     @Binding var checklist: Checklist
     
-    //ability to interact with the checklist, i.e. tick and untick items
-    
     @State var myTitle = "Checklist"
     
+    @State var loading = false // state variable to indicate loading status
+    
     var body: some View {
-        
-        List{
-            ForEach($checklist.items, id: \.self) { $pp in
-                HStack {
-                    Text(pp.item).onTapGesture {
-                        if(pp.checkedStatus == "") {
-                            pp.checkedStatus = "checkmark.circle"
-                            pp.newCheckedStatus = "checkmark.circle"
-                        }
-                        else if(pp.newCheckedStatus == "checkmark.circle"){
-                            pp.newCheckedStatus = ""
-                        }
-                    }
-                    
-                    Image(systemName: pp.newCheckedStatus)
-                    
-                }
-                
-            }
-            
-            //Also enable the user to delete an item from Detail Views through a standard swipe gesture (without needing to press Edit first).
-            
-            .onDelete { idxx in
-                checklist.items.remove(atOffsets: idxx)
+        Group {
+            if loading { // show ProgressView if loading is true
+                ProgressView("Loading...")
+                    .progressViewStyle(CircularProgressViewStyle())
+            } else { // show List if loading is false
+                List{
+                    ForEach($checklist.items, id: \.self) { $pp in
+                        HStack {
+                            Text(pp.item).onTapGesture {
+                                if(pp.checkedStatus == "") {
+                                    pp.checkedStatus = "checkmark.circle"
+                                    pp.newCheckedStatus = "checkmark.circle"
+                                }
+                                else if(pp.newCheckedStatus == "checkmark.circle"){
+                                    pp.newCheckedStatus = ""
+                                }
+                            }
 
-            //checklists on the Detail View can be re-ordered when in Edit mode
-                
-            }.onMove {indecss, poss in
-                checklist.items.move(fromOffsets: indecss, toOffset: poss)
+                            Image(systemName: pp.newCheckedStatus)
+
+                        }
+
+                    }
+
+                    .onDelete { idxx in
+                        checklist.items.remove(atOffsets: idxx)
+
+                    }.onMove {indecss, poss in
+                        checklist.items.move(fromOffsets: indecss, toOffset: poss)
+                    }
+
+                }
+                .navigationTitle(myTitle)
+                            .navigationBarItems(
+                            leading: EditButton(),
+                            trailing: Button("+") {
+                                checklist.items.append(Items(item: "New Item", checkedStatus: "", newCheckedStatus: ""))
+                            }
+                            
+                            )
             }
             
+            Button("Reset") {
+                for index in checklist.items.indices {
+                    checklist.items[index].newCheckedStatus = ""
+                }
+            }
+
+            Button("Undo") {
+                for index in checklist.items.indices {
+                    checklist.items[index].newCheckedStatus = checklist.items[index].checkedStatus
+                }
+            }
         }
-        .navigationTitle(myTitle)
-            .navigationBarItems(
-            leading: EditButton(),
-            trailing: Button("+") {
-                checklist.items.append(Items(item: "New Item", checkedStatus: "", newCheckedStatus: ""))
+        .onAppear {
+            // start loading
+            loading = true
+            // simulate loading delay
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                // stop loading after delay
+                loading = false
             }
-            
-            )
-
-        // resetting the check status of all items (i.e. unchecking all items).
-        
-                Button("Reset") {
-                    for index in checklist.items.indices {
-                        checklist.items[index].newCheckedStatus = ""
-                    }
-                }
-
-        //Ensure that there is an undo function that restores the status of the ticked-off items to originItem, in case the user hit the Reset button by accident.
-        
-                Button("Undo") {
-                    for index in checklist.items.indices {
-                        checklist.items[index].newCheckedStatus = checklist.items[index].checkedStatus
-                    }
-                }
+        }
     }
 }
